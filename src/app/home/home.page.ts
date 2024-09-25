54b815a3cb96f8cea25b9204b8a7ae5c329e98ec
RodBello <rod.bello@duocuc.cl>
2024-09-25 00:39:22 -0300

    Se creo un constructor con las variables de usuario y contraseña, incluyendo una funcion para validar el formulario

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
+  declare nombre:string;
+  declare password: string;
+  
 
   constructor() {}
 
+  onSubmit() {
+    console.log('Formulario enviado!');
+    console.log('Nombre:', this.nombre);
+    console.log('Email:', this.password);
+  }
 }
