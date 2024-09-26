bf5f831dfc98907bcdb779242f054b3ff2d786a9
RodBello <rod.bello@duocuc.cl>
2024-09-25 22:48:49 -0300

    Se agregaron asignaturas al pagina 'asignatura', mas de lo mismo en 'Asistencia', se mejoro el login y se le agergo un fondo, se crearon funciones y constructores en el ts y se agregaron estilos al SCSS de home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component} from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,15 +9,69 @@ import { Component} from '@angular/core';
 })
 
 export class HomePage {
-  declare nombre:string;
-  declare password: string;
+  username: string = '';
+  password: string = '';
+  passwordType: string = 'password';
+  tempUsername: string = 'Usuario1';
+  tempPassword: string = 'MiClav3';
+
+  constructor(private alertController: AlertController, private router: Router) {}
+
+  
+  onSubmit() {
+    if (this.username === this.tempUsername && this.password === this.tempPassword) {
+      this.showAlert('Login Exitoso', 'Has iniciado sesión correctamente.');
+      this.router.navigate(['/menu']);
+    } else {
+      this.showAlert('Error', 'Usuario o contraseña incorrectos.');
+    }
+  }
+
+  togglePasswordVisibility() {
+    this.passwordType = this.passwordType === 'password' ? 'text' : 'password';
+  }
+
   
 
-  constructor() {}
+  async showAlert(header: string, message: string) {
+    const alert = await this.alertController.create({
+      header: header,
+      message: message,
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
 
-  onSubmit() {
-    console.log('Formulario enviado!');
-    console.log('Nombre:', this.nombre);
-    console.log('Email:', this.password);
+  async resetPassword() {
+    const alert = await this.alertController.create({
+      header: 'Restablecer Contraseña',
+      inputs: [
+        {
+          name: 'newPassword',
+          type: 'password',
+          placeholder: 'Nueva Contraseña'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+        },
+        {
+          text: 'Aceptar',
+          handler: (data) => {
+            if (data.newPassword.trim() === '') {
+              this.showAlert('Error', 'No se permite una contraseña vacía');
+            } else {
+
+                this.tempPassword = data.newPassword;
+                this.showAlert('Éxito', 'Contraseña restablecida correctamente.');     
+            }
+          }
+        }
+      ]
+    });
+    await alert.present();
   }
 }
